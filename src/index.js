2fbf4c45df0d861f183d19bd7239d4ba3678b7c7
Kritskii-A <kritskii-a@bk.ru>
2020-12-16 17:08:01 +0300

    max score

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+let level; // уровень сложности
+
 // получаем canvas
 let canvas = document.getElementById("canvas");
 // получаем значение поля очков
 let result = document.getElementById("result");
 // получаем значение поля очков во всплывающем окне
 let resultScore = document.getElementById("result-modal");
+
+// получаем значение поля для макимальных результатов
+let resultScoreMax = document.getElementById("result-max");
+let resultModalScoreMax = document.getElementById("result-max-modal");
+resultScoreMax.textContent = getMaxScore();
+let maxScore = getMaxScore();
+resultModalScoreMax.textContent = maxScore;
 
 // получаем ширину и высоту экрана пользователя
 let height = document.documentElement.clientHeight;
@@ -50,17 +59,14 @@ document.getElementById("canvas").addEventListener(
   },
   false
 );
-let level;
+
 function playGameStart(levelAgain) {
   // получаем выбранный уровень
   if (levelAgain) {
     level = levelAgain;
-    console.log("true");
   } else {
     level = document.getElementById("level").value;
   }
-
-  console.log(level);
 
   // указываем точки начала
   let x_circle = randomInteger(50, width - 50),
@@ -96,6 +102,7 @@ function playGameStart(levelAgain) {
       result.textContent = score; // показываем значение во всплывающем окне
 
       clearInterval(playGame); // очищаем поле
+      changeMaxScore();
       playGameStart(level); // начинаем игру
     } else {
       click_x = 0;
@@ -114,19 +121,43 @@ function playGameStart(levelAgain) {
 
 // игра закончена
 function gameEnd(score) {
-  $("#level-again").val(level);
-  resultScore.textContent = score;
-  $("#modalEndGame").modal();
+  $("#level-again").val(level); // устанавливаем в selector текущий уровень
+  resultScore.textContent = score; // показываем очки во всплывающем окне
+  if (maxScore < score) {
+    resultModalScoreMax.textContent = score; // показываем очки во всплывающем окне
+  }
+  if (maxScore < score) {
+    setMaxScore(score); // заменяем максимальнео значение
+  }
+  $("#modalEndGame").modal(); // вызываем всплывающее окно
 }
 
 // начинаем игру сначала
 function gamePlayAgain() {
-  let levelAgain = document.getElementById("level-again").value;
-  score = 0;
-  result.textContent = score;
+  let levelAgain = document.getElementById("level-again").value; // получаем выбранный уровень игры
+  score = 0; // перезаписываем текущие очки
+  result.textContent = score; // перезаписываем текущие очки на основной странице
 
-  $("#modalEndGame").modal("hide");
-  playGameStart(levelAgain);
+  $("#modalEndGame").modal("hide"); // скрываем всплывающее окно
+  playGameStart(levelAgain); // начинаем новую игру
 }
 
+// макимальные очки запись
+function setMaxScore(score) {
+  localStorage.setItem("score", score);
+}
+
+// максимальные очки получить
+function getMaxScore() {
+  return localStorage.getItem("score");
+}
+
+// меняем максимальное значение
+function changeMaxScore() {
+  if (resultScoreMax.textContent <= score) {
+    resultScoreMax.textContent = score;
+  }
+}
+
+// показываем всплывающее окно при старте
 $("#modalHello").modal();
